0c8d2639f28b0e134170156cea422388ac13cc0f
SharwanKunwar <sharwankunwar07@gmail.com>
2025-09-13 11:16:11 +0545

    initial commit

---
+++ JavaScript/Brain.js
@@ -0,0 +1,57 @@
+//primitives data types example
+
+// let name = "sharwan";
+// let username = name;
+
+// username = "mahakal";
+
+
+// console.log(name);
+// console.log(username);
+
+
+//Reference data types
+
+// let young_age = [1,2,3,4,5];
+// let old_age = young_age;
+
+// young_age.pop();
+
+
+
+// console.log("This is young age:\n");
+// console.table(young_age);
+// console.log("\n\nThis is old age: \n")
+// console.table(old_age);
+ 
+
+
+///////////////////////////////////// NULL USE ///////////////////////////////////
+
+// let student = null;
+// console.log(student);
+// student = 22;
+// console.log(student);
+
+
+///////////////// Undefined //////////////////
+
+// let age;
+// console.log(age);
+
+
+////////////////////// symbol //////////////////////
+
+// unique immutable value
+
+
+////////////////////// bigint ///////////////////////
+
+let phone = 9999999999n;
+console.log(phone);
+
+console.log(1+"5");
+
+
+////////////////////// symbol //////////////////////
+
